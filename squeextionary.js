a9985f2efd0aba74a5f742a48d51e383ebd5f13a
driedapps <malmcbb@gmail.com>
2024-11-24 09:25:58 -0500

    updating with actual css and js files

---
+++ squeextionary.js
@@ -0,0 +1,39 @@
+const vods = [
+    {
+        filepath: 'shot1.png',
+        voddate: '10-23-2034',
+        game: 'Super Mario Party Jamboree',
+        alsoCorrect: 'Super Mario Party',
+        vodlink: 'https://www.twitch.tv/videos/2283448308'
+    },
+
+    {
+        filepath: 'shot2.png',
+        voddate: '10-02-2024',
+        game: 'UFO 50',
+        vodlink: 'https://www.twitch.tv/videos/2266307516'
+    },
+
+    {
+        filepath: 'shot3.png',
+        voddate: '09-06-2024',
+        game: 'SM64',
+        alsoCorrect: 'Super Mario 64'
+        vodlink: 'https://www.twitch.tv/videos/2243958523'
+    },
+
+    {
+        filepath: 'shot4.png',
+        voddate: '11-05-2024',
+        game: 'Rocket League',
+        vodlink: 'https://www.twitch.tv/videos/2294504340'
+    },
+
+    {
+        filepath: 'shot5.png',
+        voddate: '11-02-2022',
+        game: 'SM64',
+        alsoCorrect: 'Super Mario 64'
+        vodlink: 'https://www.twitch.tv/videos/1641776218'
+    }
+]
