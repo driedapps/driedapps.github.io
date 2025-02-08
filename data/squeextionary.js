3a6e55df7ff65dbcf073de0c16b508d79dec9345
driedapps <malmcbb@gmail.com>
2025-02-07 21:34:32 -0500

    trying bootstrap from scratch

---
+++ data/squeextionary.js
@@ -0,0 +1,29 @@
+export const vods = [
+    {
+        filepath: 'screenshots/shot2.png',
+        voddate: '10-02-2024',
+        game: ['UFO 50', 'ufo 50'],
+        vodlink: 'https://www.twitch.tv/videos/2266307516'
+    },
+
+    {
+        filepath: 'screenshots/shot3.png',
+        voddate: '09-06-2024',
+        game: ['SM64','Super Mario 64'],
+        vodlink: 'https://www.twitch.tv/videos/2243958523'
+    },
+
+    {
+        filepath: 'screenshots/shot4.png',
+        voddate: '11-05-2024',
+        game: 'Rocket League',
+        vodlink: 'https://www.twitch.tv/videos/2294504340'
+    },
+
+    {
+        filepath: 'screenshots/shot5.png',
+        voddate: '11-02-2022',
+        game: ['SM64','Super Mario 64'],
+        vodlink: 'https://www.twitch.tv/videos/1641776218'
+    }
+]
