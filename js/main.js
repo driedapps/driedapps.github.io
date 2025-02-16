eab8aa1917be4c5952c9065720eb85aca580084f
driedapps <malmcbb@gmail.com>
2025-02-16 12:18:15 -0500

    max 6 date guess disables input after

--- js/main.js
+++ js/main.js
@@ -11,12 +11,23 @@ const gameList = [
 
 ]
 
+const stats = {
+    totalDateTries: 0,
+    totalGameTries: 0,
+    guessDistribution: [0, 0, 0, 0, 0, 0],
+    currentStreak: 0,
+    maxStreak: 0
+}
+
 const vod = vods[0];
 const vodDate = new Date(vod.voddate)
 const vodYear = vodDate.getFullYear()
 const vodMonth = vodDate.getMonth() + 1
 const vodDay = vodDate.getDate()
 const vodGame = vod.game
+
+let currentTries = 0;
+const maxTries = 6;
 
 console.log(vodYear, vodMonth, vodDay, vodGame)
 
@@ -27,111 +38,126 @@ document.addEventListener("DOMContentLoaded", function() {
             let dateInput = document.getElementById('dateInput');
             let dateValue = dateInput.value;
 
+
             if (dateValue) {
-                let date = new Date(dateValue);
-                
-                let month = date.getUTCMonth() + 1
-                let day = date.getUTCDate()
-                let year = date.getUTCFullYear()
+                for (let i = 0; i < 1; i++) {
+                    if (currentTries < maxTries) {
+                        let date = new Date(dateValue);
+                        
+                        let month = date.getUTCMonth() + 1
+                        let day = date.getUTCDate()
+                        let year = date.getUTCFullYear()
 
-                let listContainer = document.getElementById('dateListContainer');
-                listContainer.style.display = 'block';
-                
-                let listGroup = document.createElement('ul');
-                listGroup.className = 'list-group list-group-horizontal-sm justify-content-center';
+                        let listContainer = document.getElementById('dateListContainer');
+                        listContainer.style.display = 'block';
+                        
+                        let listGroup = document.createElement('ul');
+                        listGroup.className = 'list-group list-group-horizontal-sm justify-content-center';
 
-                let monthDiff = Math.abs(month - vodMonth);
-                if (monthDiff > 6) {
-                    monthDiff = 12 - monthDiff; // Take the shortest distance
-                }
-                if (monthDiff === 0) {
-                    let monthItem = document.createElement('li');
-                    monthItem.className = 'list-group-item list-group-item-success';
-                    monthItem.style = "font-size: large; font-weight: bold;"
-                    monthItem.textContent = `${month}`;
-                    listGroup.appendChild(monthItem);
-                } else if (monthDiff >=1 && monthDiff <=2) {
-                    let monthItem = document.createElement('li');
-                    monthItem.className = 'list-group-item list-group-item-warning';
-                    monthItem.textContent = `${month}`;
-                    listGroup.appendChild(monthItem);
-                } else if (monthDiff >2 && monthDiff <5) {
-                    let monthItem = document.createElement('li');
-                    monthItem.className = 'list-group-item list-group-item-danger';
-                    monthItem.textContent = `${month}`;
-                    listGroup.appendChild(monthItem);
-                } else if (monthDiff >=5) {
-                    let monthItem = document.createElement('li');
-                    monthItem.className = 'list-group-item list-group-item-dark';
-                    monthItem.style = "font-weight: bold;"
-                    monthItem.textContent = `${month}`;
-                    listGroup.appendChild(monthItem);
-                }
-                
-                let dayDiff = Math.abs(day - vodDay);
+                        let monthDiff = Math.abs(month - vodMonth);
+                        if (monthDiff > 6) {
+                            monthDiff = 12 - monthDiff; // Take the shortest distance
+                        }
+                        if (monthDiff === 0) {
+                            let monthItem = document.createElement('li');
+                            monthItem.className = 'list-group-item list-group-item-success';
+                            monthItem.style = "font-size: large; font-weight: bold;"
+                            monthItem.textContent = `${month}`;
+                            listGroup.appendChild(monthItem);
+                        } else if (monthDiff >=1 && monthDiff <=2) {
+                            let monthItem = document.createElement('li');
+                            monthItem.className = 'list-group-item list-group-item-warning';
+                            monthItem.textContent = `${month}`;
+                            listGroup.appendChild(monthItem);
+                        } else if (monthDiff >2 && monthDiff <5) {
+                            let monthItem = document.createElement('li');
+                            monthItem.className = 'list-group-item list-group-item-danger';
+                            monthItem.textContent = `${month}`;
+                            listGroup.appendChild(monthItem);
+                        } else if (monthDiff >=5) {
+                            let monthItem = document.createElement('li');
+                            monthItem.className = 'list-group-item list-group-item-dark';
+                            monthItem.style = "font-weight: bold;"
+                            monthItem.textContent = `${month}`;
+                            listGroup.appendChild(monthItem);
+                        }
+                        
+                        let dayDiff = Math.abs(day - vodDay);
 
-                if (dayDiff === 0) {
-                    let dayItem = document.createElement('li');
-                    dayItem.className = 'list-group-item list-group-item-success';
-                    dayItem.style = "font-size: large; font-weight: bold;"
-                    dayItem.textContent = `${day}`;
-                    listGroup.appendChild(dayItem);
-                } else if (dayDiff >=1 && dayDiff <=3) {
-                    let dayItem = document.createElement('li');
-                    dayItem.className = 'list-group-item list-group-item-warning';
-                    dayItem.textContent = `${day}`;
-                    listGroup.appendChild(dayItem);
-                } else if (dayDiff >3 && dayDiff <8) {
-                    let dayItem = document.createElement('li');
-                    dayItem.className = 'list-group-item list-group-item-danger';
-                    dayItem.textContent = `${day}`;
-                    listGroup.appendChild(dayItem);
-                } else if (dayDiff >=8) {
-                    let dayItem = document.createElement('li');
-                    dayItem.className = 'list-group-item list-group-item-dark';
-                    dayItem.style = "font-weight: bold;"
-                    dayItem.textContent = `${day}`;
-                    listGroup.appendChild(dayItem);
-                }
-                
-                let yearDiff = Math.abs(year - vodYear);
+                        if (dayDiff === 0) {
+                            let dayItem = document.createElement('li');
+                            dayItem.className = 'list-group-item list-group-item-success';
+                            dayItem.style = "font-size: large; font-weight: bold;"
+                            dayItem.textContent = `${day}`;
+                            listGroup.appendChild(dayItem);
+                        } else if (dayDiff >=1 && dayDiff <=3) {
+                            let dayItem = document.createElement('li');
+                            dayItem.className = 'list-group-item list-group-item-warning';
+                            dayItem.textContent = `${day}`;
+                            listGroup.appendChild(dayItem);
+                        } else if (dayDiff >3 && dayDiff <8) {
+                            let dayItem = document.createElement('li');
+                            dayItem.className = 'list-group-item list-group-item-danger';
+                            dayItem.textContent = `${day}`;
+                            listGroup.appendChild(dayItem);
+                        } else if (dayDiff >=8) {
+                            let dayItem = document.createElement('li');
+                            dayItem.className = 'list-group-item list-group-item-dark';
+                            dayItem.style = "font-weight: bold;"
+                            dayItem.textContent = `${day}`;
+                            listGroup.appendChild(dayItem);
+                        }
+                        
+                        let yearDiff = Math.abs(year - vodYear);
 
-                if (yearDiff === 0) {
-                    let yearItem = document.createElement('li');
-                    yearItem.className = 'list-group-item list-group-item-success';
-                    yearItem.style = "font-size: large; font-weight: bold;"
-                    yearItem.textContent = `${year}`;
-                    listGroup.appendChild(yearItem);
-                } else if (yearDiff >=1 && yearDiff <=2) {
-                    let yearItem = document.createElement('li');
-                    yearItem.className = 'list-group-item list-group-item-warning';
-                    yearItem.textContent = `${year}`;
-                    listGroup.appendChild(yearItem);
-                } else if (yearDiff >2 && yearDiff <5) {
-                    let yearItem = document.createElement('li');
-                    yearItem.className = 'list-group-item list-group-item-danger';
-                    yearItem.textContent = `${year}`;
-                    listGroup.appendChild(yearItem);
-                } else if (yearDiff >=5) {
-                    let yearItem = document.createElement('li');
-                    yearItem.className = 'list-group-item list-group-item-dark';
-                    yearItem.style = "font-weight: bold;"
-                    yearItem.textContent = `${year}`;
-                    listGroup.appendChild(yearItem);
-                }
-                
-                listContainer.appendChild(listGroup);
-                dateInput.value = '';
+                        if (yearDiff === 0) {
+                            let yearItem = document.createElement('li');
+                            yearItem.className = 'list-group-item list-group-item-success';
+                            yearItem.style = "font-size: large; font-weight: bold;"
+                            yearItem.textContent = `${year}`;
+                            listGroup.appendChild(yearItem);
+                        } else if (yearDiff >=1 && yearDiff <=2) {
+                            let yearItem = document.createElement('li');
+                            yearItem.className = 'list-group-item list-group-item-warning';
+                            yearItem.textContent = `${year}`;
+                            listGroup.appendChild(yearItem);
+                        } else if (yearDiff >2 && yearDiff <5) {
+                            let yearItem = document.createElement('li');
+                            yearItem.className = 'list-group-item list-group-item-danger';
+                            yearItem.textContent = `${year}`;
+                            listGroup.appendChild(yearItem);
+                        } else if (yearDiff >=5) {
+                            let yearItem = document.createElement('li');
+                            yearItem.className = 'list-group-item list-group-item-dark';
+                            yearItem.style = "font-weight: bold;"
+                            yearItem.textContent = `${year}`;
+                            listGroup.appendChild(yearItem);
+                        }
+                        
+                        listContainer.appendChild(listGroup);
+                        dateInput.value = '';
+                        currentTries++;
 
-                if (yearDiff===0 && monthDiff===0 && dayDiff===0) {
-                    document.getElementById('winnersMessage').innerHTML = "You got the whole date right!!"
-                }
-
+                        if (yearDiff===0 && monthDiff===0 && dayDiff===0) {
+                            document.getElementById('winnersMessage').innerHTML = `You got the whole date right in ${currentTries} guesses!!`
+                            document.getElementById('winnersMessage').className = "text-center p-2 fs-3";
+                            document.getElementById('dateInput').disabled = true;
+                            document.getElementById('guessDate').disabled = true;
+                        }
+                    }
+            if (currentTries === 6) {
+                    document.getElementById('dateInput').disabled = true;
+                    document.getElementById('guessDate').disabled = true;
+                    document.getElementById('winnersMessage').innerHTML = `Oh... you've reached the max number of guesses for today.<br>Try again tomorrow...`;
+                    document.getElementById('winnersMessage').className = "text-center";
             }
+                }
+
+                }
             else {
                     console.warn("No date entered");
                 }
-            
+
         });
 });
 
