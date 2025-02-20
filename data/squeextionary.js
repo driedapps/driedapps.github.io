a5a48c3d08d648b5d139c6a54f233338174e2882
driedapps <malmcbb@gmail.com>
2025-02-20 13:31:03 -0500

    adding vods

--- data/squeextionary.js
+++ data/squeextionary.js
@@ -22,8 +22,140 @@ export const vods = [
 
     {
         filepath: 'screenshots/shot5.png',
-        voddate: '11-02-2022',
-        game: 'Super Mario 64',
-        vodlink: 'https://www.twitch.tv/videos/1641776218'
-    }
+        voddate: '03-11-2022',
+        game: 'Elden Ring',
+        vodlink: 'https://www.youtube.com/watch?v=hmpZNzvhW5Y'
+    },
+    {
+        filepath: 'screenshots/shot6.png',
+        voddate: '04-03-2022',
+        game: 'Kirby and the Forgotten Land',
+        vodlink: 'https://www.youtube.com/watch?v=Is5eh4ov9no'
+    },
+    {
+        filepath: 'screenshots/shot7.png',
+        voddate: '03-21-2022',
+        game: 'Elden Ring',
+        vodlink: 'https://www.youtube.com/watch?v=ur_hsLPbxaQ'
+    },
+    {
+        filepath: 'screenshots/shot8.png',
+        voddate: '04-06-2022',
+        game: 'God of War',
+        vodlink: 'https://www.youtube.com/watch?v=QKwsUzJv1Io'
+    },
+    {
+        filepath: 'screenshots/shot9.png',
+        voddate: '04-13-2022',
+        game: 'LEGO Star Wars: The Skywalker Saga Part 4',
+        vodlink: 'https://www.youtube.com/watch?v=_z-AbveKlxg'
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
+    {
+        filepath: 'screenshots/shot.png',
+        voddate: '',
+        game: '',
+        vodlink: ''
+    },
 ]
